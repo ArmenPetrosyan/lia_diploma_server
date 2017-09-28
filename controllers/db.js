531e86784a898aaebd3df79112f28cbe16071a0e
ArmenPetrosyan <thebraincrasher@gmail.com>
2017-09-28 12:36:07 +0300

    added CRUD functions

--- controllers/db.js
+++ controllers/db.js
@@ -1,5 +1,33 @@
 import mongoose from 'mongoose';
+import Note from '../models/Note';
+import config from '../config.json';
 
-import '../models/Note';
+import htmlUtils from 'js-htmlencode';
 
-const Note = mongoose.model('Note');
+const dbc = config.db;
+
+export function dbConnect(){
+	mongoose.connect(`mongodb://${dbc.host}:${dbc.port}/${dbc.name}`, { useMongoClient: true });
+}
+	
+export function getNotes() {
+	return Note.find({});
+}
+
+export function createNote({title, content, created, modified, meta}) {
+	const note = new Note({
+		content: htmlUtils.htmlEncode(content),
+		title,
+		created,
+		modified,
+		meta
+	});	
+
+	console.log(note);
+
+	return note.save();
+}
+
+export function deleteNote(id) {
+	return Note.findById(id).remove();
+}
--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import * as db from './controllers/db';
+
+import { port } from './config.json';
+
+db.dbConnect();
 
 const app = express();
+ 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-    res.send('azazaza');
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  req.header("Content-type", "application/x-www-form-urlencoded");
+  next();
 });
 
-const server = app.listen(8080, () => {
-    console.log('Server is running');
+app.get('/notes', (req, res) => {
+	db.getNotes().then( data => res.send(data) );
+});
+
+app.post('/notes', (req, res) => {
+	db.createNote(req.body).then( data => res.send(req.body) );
+});
+
+app.delete('/notes/:id', (req, res) => {
+	db.deleteNote(req.params.id).then( data => res.send(data) );
+});
+
+const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 })
